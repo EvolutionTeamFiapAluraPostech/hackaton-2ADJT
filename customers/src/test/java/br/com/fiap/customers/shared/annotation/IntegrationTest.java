74fe60f75abd85fec95cfd0506c4e8821257f7e8
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-07-28 15:03:16 -0300

    feat(customer): new configuration tests classes.

---
+++ customers/src/test/java/br/com/fiap/customers/shared/annotation/IntegrationTest.java
@@ -0,0 +1,24 @@
+package br.com.fiap.customers.shared.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@Tag("integrationTest")
+@SpringBootTest
+@AutoConfigureMockMvc
+@WithMockUser(value = "thomas.anderson@itcompany.com", authorities = {"ADMIN"})
+@ActiveProfiles("test")
+@DirtiesContext
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IntegrationTest {
+
+}

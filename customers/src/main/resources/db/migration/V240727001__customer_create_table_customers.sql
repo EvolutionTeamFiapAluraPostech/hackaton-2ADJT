4111a3c5e161d5c554dc167607a23e673c7180c4
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-07-28 15:01:54 -0300

    feat(customer): create customer table and populate customer for tests.

--- customers/src/main/resources/db/migration/V240727001__customer_create_table_customers.sql
+++ customers/src/main/resources/db/migration/V240727001__customer_create_table_customers.sql
@@ -2,19 +2,20 @@ create schema if not exists "customer_management";
 
 create table if not exists "customer_management"."customers"
 (
-    "id"          uuid                        not null default gen_random_uuid() primary key,
-    "deleted"     boolean                     not null default false,
-    "version"     bigint                      not null,
-    "created_at"  timestamp without time zone null,
-    "created_by"  varchar(255)                null,
-    "updated_at"  timestamp without time zone null,
-    "updated_by"  varchar(255)                null,
-    "name"        varchar(500)                not null,
-    "email"       varchar(500)                not null,
-    "cpf"         varchar(11)                 not null,
-    "street"      varchar(255)                not null,
-    "city"        varchar(100)                not null,
-    "state"       varchar(2)                  not null,
-    "country"     varchar(100)                not null,
-    "postal_code" varchar(8)                  not null
+    "id"           uuid                        not null default gen_random_uuid() primary key,
+    "deleted"      boolean                     not null default false,
+    "version"      bigint                      not null,
+    "created_at"   timestamp without time zone null,
+    "created_by"   varchar(255)                null,
+    "updated_at"   timestamp without time zone null,
+    "updated_by"   varchar(255)                null,
+    "name"         varchar(500)                not null,
+    "email"        varchar(500)                not null,
+    "cpf"          varchar(11)                 not null,
+    "phone_number" varchar(50)                 not null,
+    "street"       varchar(255)                null,
+    "city"         varchar(100)                null,
+    "state"        varchar(2)                  null,
+    "country"      varchar(100)                null,
+    "postal_code"  varchar(8)                  null
 );

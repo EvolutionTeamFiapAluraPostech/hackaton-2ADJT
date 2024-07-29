349af6ce34a7e221887eda85a7cd58d1e9f427f1
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-07-29 18:52:08 -0300

    config(creditcard): new project creditcard configurations.

---
+++ creditcards/src/test/java/br/com/fiap/creditcards/CreditcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.creditcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

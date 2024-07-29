283daa134d5b28962122e85622cce8289ecedb02
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-07-29 18:53:19 -0300

    feat(creditcard): credit card migrations to create creadit card table and populate initial credit card.

---
+++ creditcards/src/main/resources/db/migration/V240729002__creditcard_insert_default_creditcard_into_table_credit_cards.sql
@@ -0,0 +1,5 @@
+insert into "creditcard_management"."credit_cards"
+ ("id", "deleted", "version", "created_at", "created_by", "cpf", "limit", "number", "expiration_date",
+ "cvv")
+values ('fc9ca35f-e911-4bdd-9b73-a6ddc622d0fd', false, 0, now(), 'system', '84527263846',
+        1000.00, '1234567890123456', '12/24', '123');
